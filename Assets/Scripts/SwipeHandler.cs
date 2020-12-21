69253068aada1b36441e7184dcebd90cd5f43182
Alex Oshin <alexoshin@gatech.edu>
2020-12-21 16:47:44 -0500

    Update swipe to work with speed

--- Assets/Scripts/SwipeHandler.cs
+++ Assets/Scripts/SwipeHandler.cs
@@ -5,31 +5,38 @@ using UnityEngine.EventSystems;
 
 public class SwipeHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private Vector3 viewLocation;
-    public float swipeThreshold = 0.2f;
-    public float easingTime = 0.5f;
+    public float swipePercentThreshold = 0.2f;
+    public float swipeSpeedThreshold = 1000f;
+    public float easingTime = 0.25f;
 
-    // Start is called before the first frame update
+    private Vector3 viewLocation;
+    private float swipeStartTime = 0f;
+
     void Start()
     {
         viewLocation = transform.position;
     }
 
     public void OnBeginDrag(PointerEventData data) {
-        
+        swipeStartTime = Time.time;
     }
 
     public void OnDrag(PointerEventData data) {
-        
+        float diff = data.position.x - data.pressPosition.x;
+        this.transform.position = viewLocation + new Vector3(diff, 0, 0);
     }
 
     public void OnEndDrag(PointerEventData data) {
-        float swipePercent = (data.pressPosition.x - data.position.x) / Screen.width;
-        if (Mathf.Abs(swipePercent) >= swipeThreshold) {
+        float swipePercent = (data.position.x - data.pressPosition.x) / Screen.width;
+        float swipeTime = Time.time - swipeStartTime;
+        float swipeSpeed = Mathf.Abs(data.position.x - data.pressPosition.x) / swipeTime;
+        if (Mathf.Abs(swipePercent) >= swipePercentThreshold || swipeSpeed >= swipeSpeedThreshold) {
             Vector3 newLocation = viewLocation;
-            if (swipePercent > 0) {
+            if (swipePercent < 0) {
+                // Swipe to the left, move screen right
                 newLocation += new Vector3(-Screen.width, 0, 0);
-            } else if (swipePercent < 0) {
+            } else if (swipePercent > 0) {
+                // Swipe to the right, move screen left
                 newLocation += new Vector3(Screen.width, 0, 0);
             }
             StartCoroutine(SmoothMove(transform.position, newLocation, easingTime));
@@ -38,7 +45,7 @@ public class SwipeHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
             StartCoroutine(SmoothMove(transform.position, viewLocation, easingTime));
         }
     }
-    
+
     IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float moveTime) {
         float t = 0f;
         while (t <= moveTime) {
