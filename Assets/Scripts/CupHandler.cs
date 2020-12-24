746e60fabdba6377772a5803343827d99d562bf9
Alex Oshin <alexoshin@gatech.edu>
2020-12-23 23:26:03 -0500

    Implement cup dragging

--- Assets/Scripts/CupHandler.cs
+++ Assets/Scripts/CupHandler.cs
@@ -6,12 +6,24 @@ public class CupHandler : MonoBehaviour
 {
     public CurrentOrderHandler currentOrderHandler;
     public Size size;
+    public GameObject cupPrefab;
+
+    private GameObject draggedCup;
 
     void OnMouseDown() {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        draggedCup = Instantiate(cupPrefab, pos, Quaternion.identity);
+    }
 
+    void OnMouseDrag() {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        draggedCup.transform.position = pos;
     }
 
     void OnMouseUp() {
+        Destroy(draggedCup);
         if (currentOrderHandler.currentOrder.size == Size.none) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
